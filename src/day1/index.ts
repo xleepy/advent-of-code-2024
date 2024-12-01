692e878796f8118ebe94333384a7c75fbd123780
Radmir Gurkov <ragux0@gmail.com>
2024-12-01 12:29:40 +0100

    finish day 1 second part

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -13,13 +13,35 @@ const columns = input.split("\n").map((line) =>
 const firstColumn = columns.map((column) => column[0]).sort();
 const secondColumn = columns.map((column) => column[1]).sort();
 
-let sum = 0;
+const firstPart = () => {
+  let sum = 0;
 
-for (let i = 0; i < firstColumn.length; i++) {
-  const first = firstColumn[i];
-  const second = secondColumn[i];
-  const distance = Math.abs(first - second);
-  sum += distance;
-}
+  for (let i = 0; i < firstColumn.length; i++) {
+    const first = firstColumn[i];
+    const second = secondColumn[i];
+    const distance = Math.abs(first - second);
+    sum += distance;
+  }
+  console.log("first part", sum);
+};
 
-console.log(sum);
+firstPart();
+
+const secondPart = () => {
+  const occurrencesInRightColumn = secondColumn.reduce<Record<number, number>>(
+    (acc, value) => {
+      acc[value] = (acc[value] || 0) + 1;
+      return acc;
+    },
+    {}
+  );
+
+  let sum = 0;
+  for (const firstColumnValue of firstColumn) {
+    sum += firstColumnValue * (occurrencesInRightColumn[firstColumnValue] || 0);
+  }
+
+  console.log("second part", sum);
+};
+
+secondPart();
